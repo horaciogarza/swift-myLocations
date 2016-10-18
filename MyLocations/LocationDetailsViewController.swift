451a3f10d066e1ef691ac54a989e012839c69118
kickinbahk <kickinbahk@gmail.com>
2016-10-18 08:30:35 -0700

    fix address label to resize based on word wrapped address

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -36,6 +36,23 @@ class LocationDetailsViewController: UITableViewController {
     dateLabel.text = format(date: Date())
   }
   
+  // MARK: - UITableViewDelegate
+  override func tableView(_ tableView: UITableView,
+                          heightForRowAt indexPath: IndexPath) -> CGFloat {
+    if indexPath.section == 0 && indexPath.row == 0 {
+      return 88
+    } else if indexPath.section == 2 && indexPath.row == 2 {
+      addressLabel.frame.size = CGSize(width: view.bounds.size.width - 115,
+                                       height: 10000)
+      addressLabel.sizeToFit()
+      addressLabel.frame.origin.x = view.bounds.size.width -
+                                    addressLabel.frame.size.width - 15
+      return addressLabel.frame.size.height + 20
+    } else {
+      return 44
+    }
+  }
+  
   @IBAction func done() {
     dismiss(animated: true, completion: nil)   
   }
@@ -71,4 +88,6 @@ class LocationDetailsViewController: UITableViewController {
   func format(date: Date) -> String {
     return dateFormatter.string(from: date)
   }
+  
+
 }
