3336ea1e2e2e1729546350b25a1a2844090fb814
kickinbahk <kickinbahk@gmail.com>
2016-10-20 07:34:07 -0700

    fix error in data model definition

--- MyLocations/Location+CoreDataProperties.swift
+++ MyLocations/Location+CoreDataProperties.swift
@@ -1,5 +1,6 @@
 import Foundation
 import CoreData
+import CoreLocation
 
 
 extension Location {
@@ -10,9 +11,9 @@ extension Location {
 
     @NSManaged public var latitude: Double
     @NSManaged public var longitude: Double
-    @NSManaged public var date: NSDate?
-    @NSManaged public var locationDescription: String?
-    @NSManaged public var category: String?
-    @NSManaged public var placemark: NSObject?
+    @NSManaged public var date: Date
+    @NSManaged public var locationDescription: String
+    @NSManaged public var category: String
+    @NSManaged public var placemark: CLPlacemark?
 
 }
