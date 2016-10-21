fb8b0816559062bb71014825026976a3c8918226
kickinbahk <kickinbahk@gmail.com>
2016-10-20 17:17:16 -0700

    allow tapping on item to edit

--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -24,6 +24,21 @@ class LocationsViewController: UITableViewController {
     }
   }
   
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "EditLocation" {
+      let navigationController = segue.destination as! UINavigationController
+      
+      let controller = navigationController.topViewController
+                                          as! LocationDetailsViewController
+      controller.managedObjectContext = managedObjectContext
+      
+      if let indexPath = tableView.indexPath(for: sender as! UITableViewCell) {
+        let location = locations[indexPath.row]
+        controller.locationToEdit = location
+      }
+    }
+  }
+  
   // MARK: - UITableViewDataSource
   
   override func tableView(_ tableView: UITableView,
